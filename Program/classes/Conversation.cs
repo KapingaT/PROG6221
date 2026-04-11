7f3db59debe74e0706db3afe924d23b4e12fb9fa
KapingaT <tshilumbaastrid04@gmail.com>
2026-04-11 19:22:15 +0200

    Add files via upload

---
+++ Program/classes/Conversation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poepart1.Program.classes
+{
+    internal class Conversation
+    {
+     // this is the behind the scenes where were based on the what the user asks the bot she knows how to resond 
+        public static string GetResponse(string input, string userName) //gets the users name that we stored 
+        {
+            input = input.ToLower();
+            // convertts  into small letters 
+
+            switch (input) // the chatbot goes through all of these different choices to see wheter you question lines up to one of the following
+                // and will give you an answer based on
+            {//here are all the possible questions that the ways the peopl
+                case "hello":
+                case "hi":
+                case "hey":
+                    return "Hello " + userName + "! How can I assist you today?";
+
+                case "how are you":
+               case "how you feeling":
+                    return "I happy that you are here learning how to be  stay safe online!";
+
+                case "what is phishing":
+                    return "Phishing is when attackers trick you into giving personal information through fake emails or websites.";
+
+                case "password safety":
+                    return "Use strong passwords with letters, numbers, and special characters . Never share your password. eg One23!";
+
+                case "safe browsing":
+                    return "Always check website URLs and avoid clicking suspicious links .";
+
+                case "bye":
+                    return "Goodbye " + userName + "! Stay safe online.";
+
+                default:
+                    return "I didn’t understand that. Try asking about cybersecurity topics like phishing or passwords.";
+            }
+        }
+    }
+
+
+}
+
---
+++ Program/classes/audio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Text;
+
+namespace poepart1.Program.classes
+{
+    internal class Audio
+    {
+     
+        public static bool IsValidInput(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input);
+        }
+
+        public static void PlayGreeting()
+        {
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"voicegreeting","C:\\Users\\HP\\source\\repos\\icetask1\\icetask1\\voicegreeting\\WhatsApp Audio 2026-04-07 at 19.30.19 voice chatbox.wav");
+                SoundPlayer player = new SoundPlayer(path);
+                player.Play();
+            }
+            catch
+            {
+                Console.WriteLine("(Audio isnt playing)");
+            }
+        }
+    }
+}
+
