85ddc4f732243c0a89e0aa435f60a4fabffef3f4
KapingaT <tshilumbaastrid04@gmail.com>
2026-04-11 14:11:27 +0200

    Add files via upload

---
+++ icetask1/icetask1/Program.cs
@@ -0,0 +1,24 @@
+﻿using poepart1.Program.classes;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 1. Display header
+        // ✔ Show display first
+        Display.DisplayHeader();
+
+        // ✔ Play voice first
+        Audio.PlayGreeting();
+
+        // ✔ Start chatbot
+        Chatbox chat = new Chatbox();
+        chat.GetName();
+
+        Console.ReadLine();
+
+
+        
+    }
+}
