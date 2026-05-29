df499eaf615d69d046a1c6c9d221c26495d5acce
KapingaT <tshilumbaastrid04@gmail.com>
2026-05-29 12:58:44 +0200

    Add files via upload

---
+++ ProgP2 chatbox/ProgP2 chatbox/ProgP2 chatbox/ProgP2 chatbox/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ProgP2_chatbox
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ ProgP2 chatbox/ProgP2 chatbox/ProgP2 chatbox/ProgP2 chatbox/Chatbox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgP2_chatbox
+{
+  
+        internal class Chatbox
+        {
+            private string userName;
+
+            public void GetName()
+            {
+                // For WPF, name is collected in MainWindow
+                // This method is kept for console compatibility
+            }
+
+            private void StartChat()
+            {
+                // For WPF, chat is handled in MainWindow
+            }
+
+            private string GenerateResponse(string input)
+            {
+                return Conversation.GetResponse(input, userName);
+            }
+        }
+    }
+
